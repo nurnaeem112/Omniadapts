50d36da07c1e243411e22d2cba924a71ca98581b
nurnaeem112 <nurnaeem112@gmail.com>
2026-04-18 22:18:27 +0600

    cleanup: remove unused backup and scratch folders

--- scratch/polar_test.ts
+++
@@ -1,9 +0,0 @@
-import { Polar } from '@polar-sh/sdk';
-
-const polar = new Polar({ accessToken: 'test' });
-polar.checkouts.create({
-  products: ['test'],
-  successUrl: 'test',
-  customerEmail: 'test',
-  metadata: { userId: '123' }
-});
